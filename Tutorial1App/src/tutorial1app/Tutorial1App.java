5acdf695bcd94b99177891e087c2fe4794d0743c
DPPhemeloM8 <phemelom02@gmail.com>
2025-08-07 14:05:21 +0200

    Add files via upload

---
+++ Tutorial1App/src/tutorial1app/Tutorial1App.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tutorial1app;
+
+import za.ac.tut.ui.Tutorial1Frame;
+
+/**
+ *
+ * @author Phemelo Motebejane
+ */
+public class Tutorial1App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Tutorial1Frame tut1;//declare variables
+        tut1=new Tutorial1Frame();//instantiate frame
+        
+    }
+    
+}
